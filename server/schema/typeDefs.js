754a4c03b26be3cc5bfb10c897120786a3f9e7bc
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-08-30 15:06:52 -0500

    build typeDefs script

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,47 @@
+const typeDefs = `
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID,
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        User(_id: ID, username: String): User
+        me: User
+    }
+
+    type Mutation {
+        login(username: String, email: String, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(Book: BookInput): User
+        removeBook(bookId: ID!): User
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String
+        bookId: ID
+        image: String
+        link: String
+        title: String
+    }
+`
+
+module.exports = typeDefs;
