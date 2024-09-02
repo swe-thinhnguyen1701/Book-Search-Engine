2a39c865a036013fddbd670373e5515e93ca423e
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-01 22:42:30 -0500

    fix spelling

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,4 +1,4 @@
-const resolver = require("./resolvers");
+const resolvers = require("./resolvers");
 const typeDefs = require("./typeDefs");
 
-module.exports = { resolver, typeDefs };
+module.exports = { resolvers, typeDefs };
