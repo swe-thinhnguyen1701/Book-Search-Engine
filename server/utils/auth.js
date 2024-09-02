21950672b4b9f0d3ee9542c10ec6eff79fb18430
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-01 22:45:22 -0500

    minor update

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,6 @@
 const { GraphQLError } = require("graphql");
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
@@ -11,35 +10,28 @@ module.exports = {
       code: "UNAUTHENTICATED"
     }
   }),
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+  authMiddleware: function ({ req }) {
     let token = req.query.token || req.headers.authorization || req.body.token;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
