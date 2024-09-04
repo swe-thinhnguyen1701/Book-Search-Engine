bc4d45b84c1256c53fe48b39f168d3a023494d35
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-04 17:42:36 -0500

    update atlas IP

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,6 @@
 require('dotenv').config({ path: '/etc/secrets/mongouri' });
 const mongoose = require('mongoose');
 
-console.log("process.env.MONGODB_URI :>>", process.env.MONGODB_URI);
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
