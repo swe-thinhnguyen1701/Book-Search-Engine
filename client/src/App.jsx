54a73bae9c759849bf85c0f3ff96bcc05318641c
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-04 12:36:36 -0500

    update graphql uri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql"
+  uri: "/graphql"
 });
 
 const authLink = setContext((_, { headers }) => {
